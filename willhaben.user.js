2466a9ca706e9f99f6073a1d3fdc8a3b57295d73
Wolfram Soyka <wsoyka@users.noreply.github.com>
2019-02-28 19:24:40 +0100

    increase specificity of match

--- willhaben.user.js
+++ willhaben.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Willhaben
-// @version      0.1
+// @version      0.2
 // @description  Automatically adds Sorting, Area and Rows to Willhaben.at
 // @author       Wolfram Soyka
-// @match        http*://*.willhaben.at/iad/kaufen-und-verkaufen/*
+// @match        http*://*.willhaben.at/iad/kaufen-und-verkaufen/martkplatz/*
 // @grant        none
 // ==/UserScript==
 
@@ -19,44 +19,43 @@
 
     //only run on marketplace
 
-        //ran into issues on these pages
-        if(window.location.href.includes("contactadvertiser/confirmation")){
-            console.log("Willhaben: No sorting here");
-            return;
+    //ran into issues on these pages
+    if(window.location.href.includes("contactadvertiser/confirmation")){
+        console.log("Willhaben: No sorting here");
+        return;
+    }
+    //check if there even are GET params in the url
+    if (window.location.search.length > 0) {
+        //if theres no keyword in url, we probably havent changed the url for that keyword
+        if(!/[?&]keyword=/.test(window.location.search)){
+            sessionStorage.removeItem('changedAreaIdOnceThisSearch', true);
+        } else {
+            //if there is a keyword, make sure its not a new one. if it is, reset allowance
+            var url = new URL(window.location.href);
+            var currKw = url.searchParams.get("keyword");
+            if(currKw !== sessionStorage.getItem('lastKnownKeyword')){
+                sessionStorage.removeItem('changedUrlOnceThisSearch', true);
+            }
+            sessionStorage.setItem('lastKnownKeyword', currKw);
         }
-        //check if there even are GET params in the url
-        if (window.location.search.length > 0) {
-            //if theres no keyword in url, we probably havent changed the url for that keyword
-            if(!/[?&]keyword=/.test(window.location.search)){
-                sessionStorage.removeItem('changedAreaIdOnceThisSearch', true);
-            } else {
-                //if there is a keyword, make sure its not a new one. if it is, reset allowance
-                var url = new URL(window.location.href);
-                var currKw = url.searchParams.get("keyword");
-                if(currKw !== sessionStorage.getItem('lastKnownKeyword')){
-                    sessionStorage.removeItem('changedUrlOnceThisSearch', true);
-                }
-                sessionStorage.setItem('lastKnownKeyword', currKw);
+        if(!sessionStorage.getItem('changedUrlOnceThisSearch')){
+            var append = "";
+            //add sorting, area and rows params if not set
+            if(!/[?&]sort=/.test(window.location.search)){
+                append+="&sort="+sort;
             }
-            if(!sessionStorage.getItem('changedUrlOnceThisSearch')){
-                var append = "";
-                //add sorting, area and rows params if not set
-                if(!/[?&]sort=/.test(window.location.search)){
-                    append+="&sort="+sort;
-                }
-                if(!/[?&]areaId=/.test(window.location.search)){
-                    append+="&areaId="+area;
-                }
-                if(!/[?&]rows=/.test(window.location.search)){
-                    append+="&rows="+rows;
-                }
-                window.location.search+=append;
-                sessionStorage.setItem('changedUrlOnceThisSearch', true);
+            if(!/[?&]areaId=/.test(window.location.search)){
+                append+="&areaId="+area;
             }
-        } else { //no GET params, set everything once
-            window.location.search+="?areaId="+area+"&rows="+rows+"&sort="+sort;
+            if(!/[?&]rows=/.test(window.location.search)){
+                append+="&rows="+rows;
+            }
+            window.location.search+=append;
             sessionStorage.setItem('changedUrlOnceThisSearch', true);
         }
+    } else { //no GET params, set everything once
+        window.location.search+="?areaId="+area+"&rows="+rows+"&sort="+sort;
+        sessionStorage.setItem('changedUrlOnceThisSearch', true);
+    }
 
-    //console.log("Willhaben: "+window.location.search);
 })();
