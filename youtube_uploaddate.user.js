e0ff3f91537faf782c6cafa00f38fd42c01831bc
Wolfram Soyka <wsoyka@users.noreply.github.com>
2019-05-02 17:52:08 +0200

    v0.2
    *) dont show upload date on streams (would have been 'null')
    *) fix bug that just apends upload date elements after each navigation, resulting in showing wrong upload dates

--- youtube_uploaddate.user.js
+++ youtube_uploaddate.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Youtube Upload Date
-// @version      0.1
+// @version      0.2
 // @description  Youtube stopped providing upload dates in watch next list. This script tries to undo that.
 // @author       Wolfram Soyka
 // @match        http*://*.youtube.com/*
@@ -18,13 +18,25 @@
     'use strict';
     var $ = window.jQuery;
 
+    //?: = non capturing group, year[s],... ago
     var reg_upload = /[0-9]* (?:years?|months?|weeks?|days?) ago/;
 
     function setUpload(){
         $('ytd-watch-next-secondary-results-renderer #items #dismissable').each(function(){
             var $vidtitle = $(this).find('#video-title');
             var upload_date = reg_upload.exec($vidtitle.attr('aria-label'));
-            $("<br/><span>"+upload_date+"</span>").appendTo($(this).find('#metadata-line > span'));
+            if(upload_date !== null){ //probably a live stream
+                $("<br class='remOnReload123123'/><span class='remOnReload123123'>"+upload_date+"</span>").appendTo($(this).find('#metadata-line > span'));
+            }
+        });
+    }
+
+    function clearUpload(){
+        $('ytd-watch-next-secondary-results-renderer #items #dismissable').each(function(){
+            //$(this).remove('.remOnReload123123');
+            $(this).find('.remOnReload123123').each(function(){
+                $(this).remove();
+            });
         });
     }
 
@@ -32,10 +44,12 @@
     document.getElementsByTagName("body")[0].addEventListener("yt-navigate-finish", function(){
         setTimeout(function(){
             //inital run
+            clearUpload();
             setUpload();
             //observe
             var mutationObserver = new MutationObserver(function(mutations) {
                 mutations.forEach(function(mutation) {
+                    clearUpload();
                     setUpload();
                 });
             });
